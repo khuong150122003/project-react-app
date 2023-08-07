878a713b3d7cc5bd16f99b7beb50362bedc5d4a1
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2023-08-07 13:55:16 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Layout/Header';
+import { Box, Container, Stack } from '@mui/material';
+import Dashboard from './components/Layout/Dashboard';
+import Courses from './components/Layout/Courses';
+import { Route, Routes } from 'react-router-dom';
+import Coaching from './components/Layout/Coaching';
+import Advisory from './components/Layout/Advisory';
+import Sidebar from './components/Layout/SideBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Header/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+
+    <Sidebar/>
+    <Routes>
+      <Route path="/Dashboard" element={<Dashboard/>}/>
+      <Route path="/Courses" element={<Courses/>}/>
+      <Route path="/Coaching" element={<Coaching/>}/>
+      <Route path="/Advisory" element={<Advisory/>}/>
+
+
+    </Routes>
+
+
+      </Stack>
+    </Box>
+    
   );
 }
+
+
 
 export default App;
